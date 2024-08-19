8c4ffac6337e4197b4747f8bf14af8146b771d5a
unknown <lesleyjosh>
2024-08-19 15:43:28 +0800

    astig

---
+++ dist/js/slider.js
@@ -0,0 +1,10 @@
+const slider = tns({
+    container: ".feedback__slider",
+    items: 1,
+    gutter: 100,
+    slideBy: "page",
+    autoplay: true,
+    controls: false,
+    nav: false,
+    autoplayButtonOutput: false,
+}); 
